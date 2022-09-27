f3f2e7be5688942218e768df4c87203787d064a3
Bishesh Tilara <bisheshtilara@gmail.com>
2022-09-27 23:53:17 +0200

    Added Authentication routes and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import AuthenticationLayout from "./layouts/AuthenticationLayout";
 interface IAppProps {}
 
 const App: React.FC<IAppProps> = ({}) => {
-  return <div>Home</div>;
+  return (
+    <Routes>
+      <Route element={<AuthenticationLayout />}>
+        <Route path="*" element={<Navigate to="/signin" />} />
+        <Route path="signin" element={<div>Signin</div>} />
+        <Route path="signup" element={<div>Signup</div>} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
