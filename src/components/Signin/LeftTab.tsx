63f207a883b3b6d1229a181b02ed4b2758599640
Bishesh Tilara <bisheshtilara@gmail.com>
2022-09-30 23:57:11 +0200

    Right Tab design complete, needs refactoring

--- src/components/Signin/LeftTab.tsx
+++ src/components/Signin/LeftTab.tsx
@@ -5,13 +5,13 @@ interface ILeftTabProps {}
 
 const LeftTab: React.FC<ILeftTabProps> = ({}) => {
   return (
-    <div className="w-1/2 bg-auth bg-cover bg-center text-white flex flex-col items-center backdrop-blur-3xl justify-start rounded-tl-2xl rounded-bl-2xl">
-      <img src={appLogo} alt="1Click" className="w-64 pb-6" />
-      <div className="flex flex-col space-y-6 items-center ">
-        <p className="text-5xl font-bold">
+    <div className="w-1/2 bg-auth bg-cover bg-center text-white flex flex-col items-center backdrop-blur-3xl justify-center rounded-tl-2xl rounded-bl-2xl">
+      <img src={appLogo} alt="1Click" className="w-52" />
+      <div className="flex flex-col space-y-2 items-center pb-24">
+        <p className="text-4xl font-bold">
           Welcome to <span className="text-black">1Click</span>
         </p>
-        <p className="text-black text-2xl ">Sign in to continue</p>
+        <p className="text-black text-xl ">Sign in to continue</p>
       </div>
     </div>
   );
--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -7,7 +7,7 @@ interface ISigninProps {}
 const Signin: React.FC<ISigninProps> = ({}) => {
   return (
     <div className="h-screen w-screen flex items-center justify-center font-poppins">
-      <div className="flex w-4/5 h-4/5 rounded-2xl shadow-lg">
+      <div className="flex w-3/5 h-3/5 rounded-2xl shadow-lg">
         <LeftTab />
         <RightTab />
       </div>
